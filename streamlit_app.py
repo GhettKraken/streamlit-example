29c8d0edb3997588fd9913a0c7c89d15e6b928c5
GhettKraken <112114174+GhettKraken@users.noreply.github.com>
2023-07-28 15:29:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,135 +1,80 @@
 import streamlit as st
-import tkinter as tk
-from tkinter import filedialog, messagebox
+import pandas as pd
+import io
 import csv
 import os
 import shutil
 
 class DataCleanserApp:
 
-    def __init__(self, window):
-        self.window = window
-        self.window.geometry("500x500")
-        self.window.title("Data Cleansing")
-        self.window.config(background="white")
-
+    def __init__(self):
         self.AcademyUserData = None
         self.ExpiredUserIDs = None
         self.ExpiredUserEmailAddresses = None
         self.data_deleted = False
 
-        self.create_widgets()
-
-    def create_widgets(self):
-        # Upload Academy User Data
-        self.upload_AcademyUserData_button = tk.Button(self.window, text="Upload Academy User Data", command=self.uploadAcademyUserData)
-        self.upload_AcademyUserData_button.pack()
-        self.academy_user_data_status = tk.Label(self.window, text="")
-        self.academy_user_data_status.pack()
-
-        # Upload Expired User IDs and Email Addresses
-        self.upload_ExpiredUserIDs_button = tk.Button(self.window, text="Upload Expired User IDs", state=tk.DISABLED, command=self.uploadExpiredUserIDs)
-        self.upload_ExpiredUserIDs_button.pack()
-        self.expired_user_ids_status = tk.Label(self.window, text="")
-        self.expired_user_ids_status.pack()
-
-        self.upload_ExpiredEmails_button = tk.Button(self.window, text="Upload Expired User Email Addresses", state=tk.DISABLED, command=self.uploadExpiredUserEmailAddresses)
-        self.upload_ExpiredEmails_button.pack()
-        self.expired_user_emails_status = tk.Label(self.window, text="")
-        self.expired_user_emails_status.pack()
-
-        # Delete Expired User Data
-        self.delete_expiredIDs_button = tk.Button(self.window, text="Delete Expired User IDs", state=tk.DISABLED, command=self.deleteExpiredUserIDs)
-        self.delete_expiredIDs_button.pack()
-        self.delete_expired_ids_status = tk.Label(self.window, text="")
-        self.delete_expired_ids_status.pack()
-
-        self.delete_expiredEmails_button = tk.Button(self.window, text="Delete Expired User Email Addresses", state=tk.DISABLED, command=self.deleteExpiredUserEmailAddresses)
-        self.delete_expiredEmails_button.pack()
-        self.delete_expired_emails_status = tk.Label(self.window, text="")
-        self.delete_expired_emails_status.pack()
-
-        # Export Cleansed Data
-        self.export_button = tk.Button(self.window, text="Export Cleansed Academy User Data", state=tk.DISABLED, command=self.exportCleansedAcademyUserData)
-        self.export_button.pack()
-        self.export_status = tk.Label(self.window, text="")
-        self.export_status.pack()
-
-        # Exit Program
-        self.exit_button = tk.Button(self.window, text="Exit Program", command=self.exitProgram)
-        self.exit_button.pack()
-
     def uploadAcademyUserData(self):
-        self.AcademyUserData = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("CSV Files", "*.csv*"), ("all files", "*.*")))
-        if self.AcademyUserData:
-            self.upload_ExpiredUserIDs_button.config(state=tk.NORMAL)
-            self.upload_ExpiredEmails_button.config(state=tk.NORMAL)
-            self.academy_user_data_status.config(text="Academy User Data Uploaded")
+        uploaded_file = st.file_uploader("Upload Academy User Data", type="csv")
+        if uploaded_file is not None:
+            self.AcademyUserData = pd.read_csv(uploaded_file)
+            st.write("Academy User Data Uploaded")
         else:
-            self.academy_user_data_status.config(text="Please Upload a File")
+            st.write("Please Upload a File")
 
     def uploadExpiredUserIDs(self):
-        self.ExpiredUserIDs = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("CSV Files", "*.csv*"), ("all files", "*.*")))
-        if self.ExpiredUserIDs:
-            self.delete_expiredIDs_button.config(state=tk.NORMAL)
-            self.expired_user_ids_status.config(text="Expired User IDs Uploaded")
+        uploaded_file = st.file_uploader("Upload Expired User IDs", type="csv")
+        if uploaded_file is not None:
+            self.ExpiredUserIDs = pd.read_csv(uploaded_file)
+            st.write("Expired User IDs Uploaded")
         else:
-            self.expired_user_ids_status.config(text="Please Upload a File")
+            st.write("Please Upload a File")
 
     def uploadExpiredUserEmailAddresses(self):
-        self.ExpiredUserEmailAddresses = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("CSV Files", "*.csv*"), ("all files", "*.*")))
-        if self.ExpiredUserEmailAddresses:
-            self.delete_expiredEmails_button.config(state=tk.NORMAL)
-            self.expired_user_emails_status.config(text="Expired User Email Addresses Uploaded")
+        uploaded_file = st.file_uploader("Upload Expired User Email Addresses", type="csv")
+        if uploaded_file is not None:
+            self.ExpiredUserEmailAddresses = pd.read_csv(uploaded_file)
+            st.write("Expired User Email Addresses Uploaded")
         else:
-            self.expired_user_emails_status.config(text="Please Upload a File")
+            st.write("Please Upload a File")
 
     def deleteExpiredUserIDs(self):
-        self.AcademyUserData = self.deleteExpiredUsers(self.AcademyUserData, self.ExpiredUserIDs, 'User ID')
-        self.data_deleted = True
-        self.check_export_status()
-        self.delete_expired_ids_status.config(text="Expired User IDs Deleted")
+        if st.button("Delete Expired User IDs"):
+            self.AcademyUserData = self.deleteExpiredUsers(self.AcademyUserData, self.ExpiredUserIDs, 'User ID')
+            self.data_deleted = True
+            self.check_export_status()
+            st.write("Expired User IDs Deleted")
 
     def deleteExpiredUserEmailAddresses(self):
-        self.AcademyUserData = self.deleteExpiredUsers(self.AcademyUserData, self.ExpiredUserEmailAddresses, 'Email Address')
-        self.data_deleted = True
-        self.check_export_status()
-        self.delete_expired_emails_status.config(text="Expired User Email Addresses Deleted")
+        if st.button("Delete Expired User Email Addresses"):
+            self.AcademyUserData = self.deleteExpiredUsers(self.AcademyUserData, self.ExpiredUserEmailAddresses, 'Email Address')
+            self.data_deleted = True
+            self.check_export_status()
+            st.write("Expired User Email Addresses Deleted")
 
-    def deleteExpiredUsers(self, user_data_file, expired_users_file, column_name):
-        with open(user_data_file, "r") as f:
-            reader = csv.reader(f)
-            user_data = list(reader)
-            id_index = user_data[0].index(column_name)
-
-        with open(expired_users_file, "r") as f:
-            reader = csv.reader(f)
-            expired_users = list(reader)
-            expired_id_index = expired_users[0].index(column_name)
-
-        user_data = [row for row in user_data if row[id_index] not in [expired_row[expired_id_index] for expired_row in expired_users]]
-        
-        with open(user_data_file, "w", newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows(user_data)
-        
-        return user_data_file
+    def deleteExpiredUsers(self, user_data, expired_users, column_name):
+        user_data = user_data[~user_data[column_name].isin(expired_users[column_name])]
+        return user_data
 
     def exportCleansedAcademyUserData(self):
-        shutil.copy(self.AcademyUserData, "CleansedAcademyUserData.csv")
-        self.export_status.config(text="Cleansed Academy User Data Exported")
+        if st.button("Export Cleansed Academy User Data"):
+            csv = self.AcademyUserData.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}" download="CleansedAcademyUserData.csv">Download CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
 
     def check_export_status(self):
         if self.data_deleted:
             self.export_button.config(state=tk.NORMAL)
 
-    def exitProgram(self):
-        self.window.destroy()
-
 def main():
-    root = tk.Tk()
-    app = DataCleanserApp(root)
-    root.mainloop()
+    st.title("Data Cleansing")
+    app = DataCleanserApp()
+    app.uploadAcademyUserData()
+    app.uploadExpiredUserIDs()
+    app.uploadExpiredUserEmailAddresses()
+    app.deleteExpiredUserIDs()
+    app.deleteExpiredUserEmailAddresses()
+    app.exportCleansedAcademyUserData()
 
 if __name__ == "__main__":
     main()
